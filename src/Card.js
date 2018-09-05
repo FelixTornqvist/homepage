811b72d0a2b251dc369ae3b91c786be7d2a9423c
Felix Törnqvist <felix.trnqvist@gmail.com>
2018-09-05 22:42:59 +0200

    playing with styles

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+	return (
+		<div className="card">
+		this is a test
+		</div>
+	);
+  }
+}
+
+export default Card;
