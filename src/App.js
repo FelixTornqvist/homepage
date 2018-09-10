42fe45677b028a002bf941e6ab8f9f2d2821fb2a
Felix Törnqvist <felix.trnqvist@gmail.com>
2018-09-10 19:10:49 +0200

    playing with using array with components

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import Card from './Card.js';
 import './App.css';
 
 class App extends Component {
+
+	renderCard(image, text) {
+		return (<Card image={image} text={text}/>);
+	}
+
 	render() {
+		var elements = [];
+
+		for (var i = 0, len = 10; i < len; i++) {
+			elements.push(this.renderCard("https://picsum.photos/250/150", i));
+		}
 		return (
 			<div>
 				<Header/>
 				<div className="card-holder">
-					<Card image="https://picsum.photos/250/150" text="hi"/>
-					<Card image="https://picsum.photos/250/150" text="there"/>
-					<Card image="https://picsum.photos/250/150" text="nice"/>
-					<Card image="https://picsum.photos/250/150" text="to"/>
-					<Card image="https://picsum.photos/250/150" text="meet"/>
-					<Card image="https://picsum.photos/250/150" text="you"/>
+					{elements}
 				</div>
 			</div>
 		);
 	}
+
 }
 
 export default App;
